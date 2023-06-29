387a5d667fcc91ee76a951b36504cbbb828907df
sushank3 <sushankkr3@gmail.com>
2023-06-29 09:36:01 +0530

    monthlymonthly

--- src/components/Community/Premium.tsx
+++ src/components/Community/Premium.tsx
@@ -17,7 +17,7 @@ const Premium: React.FC = () => {
         <Icon as={GiCheckedShield} fontSize={26} color="brand.100" mt={2} />
         <Stack spacing={1} fontSize="9pt" pl={2}>
           <Text fontWeight={600}>Reddit Premium</Text>
-          <Text>The best Reddit experience, with monthly Coins</Text>
+          <Text>The best Reddit experience, with monthly monthlymonthly Coins</Text>
         </Stack>
       </Flex>
       <Button height="30px" bg="brand.100">
